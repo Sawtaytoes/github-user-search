f1097ef49d3970595adf5afbeb7f2b284e369685
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-10 23:11:52 -0500

    Initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+
+const App = () => (
+	<div className="App">
+		<header className="App-header">
+			<input type="text" />
+		</header>
+	</div>
+)
+
+export default App
